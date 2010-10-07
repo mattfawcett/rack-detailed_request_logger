6bc42d66eb28b9cce52de75279cf46df04391e33
Matt Fawcett <mail@matthewfawcett.co.uk>
2010-10-07 11:09:47 +0100

    Initial commit to rack-detailed_request_logger.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rack-detailed_request_logger'
+
+class Test::Unit::TestCase
+end
