cccdd21d5d24358c4efa26f530b0b3138f5c6338
Matt Fawcett <mail@matthewfawcett.co.uk>
2010-10-07 14:22:15 +0100

    Fixed bug with the post data being removed by the logger

--- lib/rack/detailed_request_logger.rb
+++ lib/rack/detailed_request_logger.rb
@@ -6,12 +6,19 @@ module Rack
     end
 
     def call(env)
+      # generate log data
+      post_data = env["rack.input"].gets
       to = "To: #{env["REQUEST_METHOD"]} #{env["PATH_INFO"]}"
       to << "?#{env["QUERY_STRING"]}" unless env["QUERY_STRING"].nil? || env["QUERY_STRING"].blank?
       entry = ["From IP: #{env['HTTP_X_FORWARDED_FOR'] || env["REMOTE_ADDR"] || "-"}"]
       entry << to
-      entry << "Raw POST Data: #{env["rack.input"].gets}"
+      entry << "Raw POST Data: #{post_data}"
       @logger.info entry.join(", ")
+      
+      # restore the value of env["rack.input"]
+      env["rack.input"] = StringIO.new(post_data) if post_data
+      
+      # return the app
       status, header, body = @app.call(env)
       [status, header, body]
     end
